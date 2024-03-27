24674aa80e36553878a95bfbdb84032b66b06bdb
cgil <lao.tseu.is.alive@gmail.com>
2024-03-27 16:29:38 +0100

    needed to migrate go version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goCloudK8sThing"
 	AppSnake   = "go-cloud-k8s-thing"
-	VERSION    = "0.0.8"
+	VERSION    = "0.0.9"
 	REPOSITORY = "github.com/lao-tseu-is-alive/go-cloud-k8s-thing"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
